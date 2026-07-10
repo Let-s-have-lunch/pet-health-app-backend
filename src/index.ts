08bee5dbc27bf7df8ae027e6068d6ec3e7c212ae
yeonhan <yeonhanlab@gmail.com>
2026-07-10 11:23:27 +0900

    cors 수정

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ const app = express();
 
 const PORT = process.env.PORT || "8080";
 
-app.use(cors({ origin: ["http://localhost:8081", "http://localhost:8082"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:8081", "http://localhost:8082", "http://localhost:8083",], credentials: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
